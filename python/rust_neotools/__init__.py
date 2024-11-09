a836d6fa06d8948c14f49d358a819af9f425b02e
Steve C <diceroll123@gmail.com>
2024-11-08 22:30:45 -0500

    build with uv (#9)

--- python/rust_neotools/__init__.py
+++ python/rust_neotools/__init__.py
@@ -2,9 +2,9 @@ __title__ = "rust_neotools"
 __author__ = "diceroll123"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023-present diceroll123"
-__version__ = "0.0.1a"
+__version__ = "0.0.3"
 
-from .rust_neotools import IslandMystic, Symol
+from rust_neotools import IslandMystic, Symol
 
 __all__ = (
     "IslandMystic",
